b68d3ad934f950d1d6f89638d1ed43d58668b574
Roukys <73461293+Roukys@users.noreply.github.com>
2020-10-27 09:10:37 +0100

    Update HHAuto.user.js

--- HHAuto.user.js
+++ HHAuto.user.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name         HaremHeroes Automatic++
 // @namespace    https://github.com/Roukys/HHauto
-// @version      5
+// @version      5b1
 // @description  Open the menu in HaremHeroes(topright) to toggle AutoControlls. Supports AutoSalary, AutoContest, AutoMission, AutoQuest, AutoTrollBattle, AutoArenaBattle and AutoPachinko(Free), AutoLeagues, AutoChampions and AutoStatUpgrades. Messages are printed in local console.
 // @author       JD and Dorten(a bit) and roukys
 // @match        http*://nutaku.haremheroes.com/*
