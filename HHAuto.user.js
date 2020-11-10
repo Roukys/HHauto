035ced8edc03cd0ec5b515ffe454bbf378a2cda9
Roukys <73461293+Roukys@users.noreply.github.com>
2020-11-10 11:22:32 +0100

    added back to home end of Pop

--- HHAuto.user.js
+++ HHAuto.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         HaremHeroes Automatic++
 // @namespace    https://github.com/Roukys/HHauto
-// @version      5.1.1.2
+// @version      5.1.1.3
 // @description  Open the menu in HaremHeroes(topright) to toggle AutoControlls. Supports AutoSalary, AutoContest, AutoMission, AutoQuest, AutoTrollBattle, AutoArenaBattle and AutoPachinko(Free), AutoLeagues, AutoChampions and AutoStatUpgrades. Messages are printed in local console.
 // @author       JD and Dorten(a bit) and roukys
 // @match        http*://nutaku.haremheroes.com/*
@@ -2962,6 +2962,7 @@ var autoLoop = function () {
                 {
                     console.log("removing popToStart");
                     Storage().removeItem('PopToStart');
+                    gotoPage("home");
                 }
             }
         }
