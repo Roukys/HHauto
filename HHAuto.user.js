0debe60fb2544e772b80b16f80e52d4ec4c88b25
Roukys <73461293+Roukys@users.noreply.github.com>
2021-05-04 19:31:33 +0200

    fix market option switch
    fix #211

--- HHAuto.user.js
+++ HHAuto.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         HaremHeroes Automatic++
 // @namespace    https://github.com/Roukys/HHauto
-// @version      5.4.22
+// @version      5.4.23
 // @description  Open the menu in HaremHeroes(topright) to toggle AutoControlls. Supports AutoSalary, AutoContest, AutoMission, AutoQuest, AutoTrollBattle, AutoArenaBattle and AutoPachinko(Free), AutoLeagues, AutoChampions and AutoStatUpgrades. Messages are printed in local console.
 // @author       JD and Dorten(a bit), roukys, cossname
 // @match        http*://nutaku.haremheroes.com/*
@@ -5328,7 +5328,7 @@ var autoLoop = function () {
     {
         moduleDisplayPopID();
     }
-    if (getPage() === "shop" )
+    if (getPage() === "shop"  && Storage().HHAuto_Setting_showMarketTools === "true")
     {
         moduleShopActions();
     }
