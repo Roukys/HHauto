108b878f5a352a54bdbb28cba515019683269ea8
Roukys <73461293+Roukys@users.noreply.github.com>
2021-06-10 11:56:09 +0200

    5.4.48 fix getPage error
    #224

--- HHAuto.user.js
+++ HHAuto.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         HaremHeroes Automatic++
 // @namespace    https://github.com/Roukys/HHauto
-// @version      5.4.47
+// @version      5.4.48
 // @description  Open the menu in HaremHeroes(topright) to toggle AutoControlls. Supports AutoSalary, AutoContest, AutoMission, AutoQuest, AutoTrollBattle, AutoArenaBattle and AutoPachinko(Free), AutoLeagues, AutoChampions and AutoStatUpgrades. Messages are printed in local console.
 // @author       JD and Dorten(a bit), Roukys, cossname, YotoTheOne
 // @match        http*://nutaku.haremheroes.com/*
@@ -679,7 +679,7 @@ function gotoPage(page,delay = -1)
         page = 'powerplace';
     }
 
-    if (typeof x != 'number')
+    if (typeof delay != 'number')
     {
         delay = -1;
     }
