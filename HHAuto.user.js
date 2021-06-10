59e089ae1f64dad99a2a52898baa9291d8c396b8
Roukys <73461293+Roukys@users.noreply.github.com>
2021-06-10 17:16:05 +0200

    5.4.49 fix delay coding issue =
    #224

--- HHAuto.user.js
+++ HHAuto.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         HaremHeroes Automatic++
 // @namespace    https://github.com/Roukys/HHauto
-// @version      5.4.48
+// @version      5.4.49
 // @description  Open the menu in HaremHeroes(topright) to toggle AutoControlls. Supports AutoSalary, AutoContest, AutoMission, AutoQuest, AutoTrollBattle, AutoArenaBattle and AutoPachinko(Free), AutoLeagues, AutoChampions and AutoStatUpgrades. Messages are printed in local console.
 // @author       JD and Dorten(a bit), Roukys, cossname, YotoTheOne
 // @match        http*://nutaku.haremheroes.com/*
@@ -683,7 +683,7 @@ function gotoPage(page,delay = -1)
     {
         delay = -1;
     }
-    if (delay = -1 )
+    if (delay === -1 )
     {
         delay = randomInterval(300,500);
     }
