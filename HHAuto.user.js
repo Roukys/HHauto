caf824785e28db44456e04f869305966d1ad9475
Roukys <73461293+Roukys@users.noreply.github.com>
2021-06-11 08:50:11 +0200

    5.4.51 add hh comix support draft

--- HHAuto.user.js
+++ HHAuto.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         HaremHeroes Automatic++
 // @namespace    https://github.com/Roukys/HHauto
-// @version      5.4.50
+// @version      5.4.51
 // @description  Open the menu in HaremHeroes(topright) to toggle AutoControlls. Supports AutoSalary, AutoContest, AutoMission, AutoQuest, AutoTrollBattle, AutoArenaBattle and AutoPachinko(Free), AutoLeagues, AutoChampions and AutoStatUpgrades. Messages are printed in local console.
 // @author       JD and Dorten(a bit), Roukys, cossname, YotoTheOne
 // @match        http*://nutaku.haremheroes.com/*
 // @match        http*://*.hentaiheroes.com/*
 // @match        http*://*.gayharem.com/*
+// @match        http*://*.comixharem.com/*
 // @grant        GM_addStyle
 // @license      MIT
 // @updateURL   https://github.com/Roukys/HHauto/raw/main/HHAuto.user.js
@@ -611,6 +612,10 @@ function getPage()
         if(ob===undefined || ob === null)
         {
             ob = document.getElementById("hh_gay");
+        }
+        if(ob===undefined || ob === null)
+        {
+            ob = document.getElementById("hh_comix");
         }
         if(ob===undefined || ob === null)
         {
