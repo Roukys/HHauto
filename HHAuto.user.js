a405347d404136c62d5949d8210a7c84b8eedada
Roukys <73461293+Roukys@users.noreply.github.com>
2022-02-23 20:16:35 +0100

    5.6.60 fix pantheon loop

--- HHAuto.user.js
+++ HHAuto.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         HaremHeroes Automatic++
 // @namespace    https://github.com/Roukys/HHauto
-// @version      5.6.59
+// @version      5.6.60
 // @description  Open the menu in HaremHeroes(topright) to toggle AutoControlls. Supports AutoSalary, AutoContest, AutoMission, AutoQuest, AutoTrollBattle, AutoArenaBattle and AutoPachinko(Free), AutoLeagues, AutoChampions and AutoStatUpgrades. Messages are printed in local console.
 // @author       JD and Dorten(a bit), Roukys, cossname, YotoTheOne, CLSchwab, deuxge
 // @match        http*://*.haremheroes.com/*
@@ -3544,7 +3544,10 @@ function doPantheon()
         if (pantheonTempleBattleButton.length >0)
         {
             //replaceCheatClick();
+            setStoredValue("HHAuto_Temp_autoLoop", "false");
+            logHHAuto("setting autoloop to false");
             pantheonTempleBattleButton[0].click();
+            setTimeout(function () {location.reload();}, 1000);
         }
         else
         {
