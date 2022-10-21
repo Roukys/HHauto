b5b734e54761fc8bca664d16fd239985edf81a95
OldRon1977 <73475830+OldRon1977@users.noreply.github.com>
2022-10-21 13:02:45 +0200

    Update HHAuto.user.js

--- HHAuto.user.js
+++ HHAuto.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         HaremHeroes Automatic++
 // @namespace    https://github.com/Roukys/HHauto
-// @version      5.6.114
+// @version      5.6.115
 // @description  Open the menu in HaremHeroes(topright) to toggle AutoControlls. Supports AutoSalary, AutoContest, AutoMission, AutoQuest, AutoTrollBattle, AutoArenaBattle and AutoPachinko(Free), AutoLeagues, AutoChampions and AutoStatUpgrades. Messages are printed in local console.
 // @author       JD and Dorten(a bit), Roukys, cossname, YotoTheOne, CLSchwab, deuxge, react31, PrimusVox
 // @match        http*://*.haremheroes.com/*
@@ -10696,8 +10696,9 @@ HHEnvVariables["SH_prod"].isEnabledPoG = false;// to remove when PoG arrives in 
                                           'Headmistress Asa Akira',
                                           'Sammy Jayne',
                                           'Ivy Winters',
-										  'Lily Cade'
-										  'Amia Miley'];
+										  'Lily Cade',
+										  'Amia Miley',
+										  'Alyssa Reece'];
     HHEnvVariables[element].isEnabledPantheon = false;// to remove when Pantheon arrives in pornstar
     HHEnvVariables[element].isEnabledPoG = false;// to remove when PoG arrives in pornstar
 })
