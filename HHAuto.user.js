e8e716bf2fb2e100a285027e943e36aff0d9c73b
jdelisse <a@a.com>
2023-04-02 15:46:22 +0200

    fix club champion #719

--- HHAuto.user.js
+++ HHAuto.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         HaremHeroes Automatic++
 // @namespace    https://github.com/Roukys/HHauto
-// @version      5.13.0
+// @version      5.13.1
 // @description  Open the menu in HaremHeroes(topright) to toggle AutoControlls. Supports AutoSalary, AutoContest, AutoMission, AutoQuest, AutoTrollBattle, AutoArenaBattle and AutoPachinko(Free), AutoLeagues, AutoChampions and AutoStatUpgrades. Messages are printed in local console.
 // @author       JD and Dorten(a bit), Roukys, cossname, YotoTheOne, CLSchwab, deuxge, react31, PrimusVox, OldRon1977, tsokh, UncleBob800
 // @match        http*://*.haremheroes.com/*
@@ -3542,6 +3542,7 @@ var doClubChampionStuff=function()
             else
             {
                 logHHAuto("Max tickets to use on Club Champ reached.");
+                setTimer('nextClubChampionTime', 60*60);
             }
 
         }
