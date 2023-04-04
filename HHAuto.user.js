3e9ef9355f25363243dd872b9e24e86868b0f6e3
PrimusVox <primusvox@outlook.com>
2023-04-04 14:45:33 -0400

    Refine PoP precision calculation
    There were cases where the previous calculation would get stuck in
    a loop or provide substandard results. This new formula should be much
    more robust.

--- HHAuto.user.js
+++ HHAuto.user.js
@@ -2408,7 +2408,9 @@ function doPowerPlacesStuff(index)
                             thisPower += girl.power;
                         });
                         // Give the team a score to try and use more efficient teams (ie: fewer girls) instead of just the fastest
-                        const thisScore = Math.min(1, ((thisPower/powerText) * ((1 / Math.sqrt(theseGirls.length))+0.28)));
+                        const kValue = 40;
+                        const xValue = thisPower / powerText;
+                        const thisScore = Math.min(1, ( (1 - Math.pow(xValue, theseGirls.length)) / (1 - Math.pow(xValue, kValue))));
                         if (thisScore > teamScore) {
                             teamScore = thisScore;
                             chosenTeam = theseGirls;
