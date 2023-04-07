799a26bc6dc3e88a546860dd5b5a18cfd406da5b
deuxge <a@a.com>
2023-04-07 15:44:35 +0200

    Add TS troll name #752

--- HHAuto.user.js
+++ HHAuto.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         HaremHeroes Automatic++
 // @namespace    https://github.com/Roukys/HHauto
-// @version      5.16.1
+// @version      5.16.2
 // @description  Open the menu in HaremHeroes(topright) to toggle AutoControlls. Supports AutoSalary, AutoContest, AutoMission, AutoQuest, AutoTrollBattle, AutoArenaBattle and AutoPachinko(Free), AutoLeagues, AutoChampions and AutoStatUpgrades. Messages are printed in local console.
 // @author       JD and Dorten(a bit), Roukys, cossname, YotoTheOne, CLSchwab, deuxge, react31, PrimusVox, OldRon1977, tsokh, UncleBob800
 // @match        http*://*.haremheroes.com/*
@@ -10693,7 +10693,8 @@ HHEnvVariables["SH_prod"].isEnabledPoG = false;// to remove when PoG arrives in 
 
 ["TPH_prod","NTPH_prod"].forEach((element) => {
     HHEnvVariables[element].trollzList = ['Latest',
-                                          'Domino Presley'];
+                                          'Domino Presley',
+                                          'Emma Rose'];
     HHEnvVariables[element].isEnabledSideQuest = false;// to remove when SideQuest arrives in transpornstar
     HHEnvVariables[element].isEnabledPowerPlaces = false;// to remove when PoP arrives in transpornstar
     HHEnvVariables[element].isEnabledAllChamps = false;// to remove when Champs arrives in transpornstar
