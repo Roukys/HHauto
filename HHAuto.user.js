50098cd2cc1f847f0aa75637467ed99a0a1a1169
deuxge <deuxge@users.noreply.github.com>
2023-05-04 10:41:48 +0200

    Trigger shop refreh when timer is over #811

--- HHAuto.user.js
+++ HHAuto.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         HaremHeroes Automatic++
 // @namespace    https://github.com/Roukys/HHauto
-// @version      5.26.5
+// @version      5.26.6
 // @description  Open the menu in HaremHeroes(topright) to toggle AutoControlls. Supports AutoSalary, AutoContest, AutoMission, AutoQuest, AutoTrollBattle, AutoArenaBattle and AutoPachinko(Free), AutoLeagues, AutoChampions and AutoStatUpgrades. Messages are printed in local console.
 // @author       JD and Dorten(a bit), Roukys, cossname, YotoTheOne, CLSchwab, deuxge, react31, PrimusVox, OldRon1977, tsokh, UncleBob800
 // @match        http*://*.haremheroes.com/*
@@ -9301,6 +9301,8 @@ function checkEvent(inEventID)
                 eventList[inEventID]["next_refresh"]<new Date()
                 ||
                 (eventType === "mythic" && checkTimerMustExist('eventMythicNextWave'))
+                || 
+                (eventType === "sultryMysteries" && checkTimerMustExist('eventSultryMysteryShopRefresh'))
                 )
             {
                 return true;
