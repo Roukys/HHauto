cb530cec98aae937f6df78332e5321290521cfcd
deuxge <deuxge@users.noreply.github.com>
2023-12-07 21:07:03 +0100

    Allow auto buy to be less than 20 #1117

--- src/Module/Events/SultryMysteries.js
+++ src/Module/Events/SultryMysteries.js
@@ -1,7 +1,6 @@
 import { getHHScriptVars, getHHVars } from "../../Helper";
 
 export class SultryMysteries {
-    
     static isEnabled(){
         return getHHVars('Hero.infos.level')>=getHHScriptVars("LEVEL_MIN_EVENT_SM");
     }
