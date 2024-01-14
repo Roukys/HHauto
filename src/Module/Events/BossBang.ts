b130a06db3c53cf8571c8f6d83f687967c7ada55
deuxge <deuxge@users.noreply.github.com>
2024-01-14 14:20:35 +0100

    Fix boss band after game update

--- src/Module/Events/BossBang.ts
+++ src/Module/Events/BossBang.ts
@@ -7,19 +7,20 @@ export class BossBang {
     static skipFightPage()
     {
         const rewardsButton = $('#rewards_popup .blue_button_L:not([disabled]):visible');
-        const skipFightButton = $('#new_battle #new-battle-skip-btn:not([disabled]):visible');
+        const skipFightButton = $('#battle #new-battle-skip-btn:not([disabled]):visible');
         if(rewardsButton.length > 0)
         {
             logHHAuto("Click get rewards bang fight");
             rewardsButton.trigger('click');
+            setStoredValue(HHStoredVarPrefixKey + "Temp_autoLoop", "false");
         }
         else if(skipFightButton.length > 0)
         {
             logHHAuto("Click skip boss bang fight");
             skipFightButton.trigger('click');
-            setTimeout(BossBang.skipFightPage,randomInterval(1300,1900));
+            setTimeout(BossBang.skipFightPage, randomInterval(1300, 1900));
+            setStoredValue(HHStoredVarPrefixKey + "Temp_autoLoop", "false");
         }
-        setStoredValue(HHStoredVarPrefixKey+"Temp_autoLoop", "false");
     }
     static goToFightPage(){
         const teamIndexFound = parseInt(getStoredValue(HHStoredVarPrefixKey+"Temp_bossBangTeam"));
