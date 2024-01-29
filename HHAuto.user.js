ebb81e2c6d0f3cc2c3be14f139c053b44e4496ac
deuxge <deuxge@users.noreply.github.com>
2024-01-29 19:18:00 +0100

    Add POA collect all button #1152

--- HHAuto.user.js
+++ HHAuto.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         HaremHeroes Automatic++
 // @namespace    https://github.com/Roukys/HHauto
-// @version      7.3.9
+// @version      7.3.10
 // @description  Open the menu in HaremHeroes(topright) to toggle AutoControlls. Supports AutoSalary, AutoContest, AutoMission, AutoQuest, AutoTrollBattle, AutoArenaBattle and AutoPachinko(Free), AutoLeagues, AutoChampions and AutoStatUpgrades. Messages are printed in local console.
 // @author       JD and Dorten(a bit), Roukys, cossname, YotoTheOne, CLSchwab, deuxge, react31, PrimusVox, OldRon1977, tsokh, UncleBob800
 // @match        http*://*.haremheroes.com/*
@@ -1695,16 +1695,13 @@ class PathOfAttraction {
     }
     static displatCollectAllButton() {
         if (PathOfAttraction.hasUnclaimedRewards() && $('#PoaCollectAll').length == 0) {
-            /*
-            // TODO update path when POA is available
-            const button = $(`<button class="purple_button_L" id="PoaCollectAll">${getTextForUI("collectAllButton", "elementText")}</button>`);
-            const divTooltip = $(`<div class="tooltipHH" style="position: absolute;top: 260px;width: 110px;font-size: small;"><span class="tooltipHHtext">${getTextForUI("collectAllButton", "tooltip")}</span></div>`);
+            const button = $(`<button class="purple_button_L" style="padding:0px 5px" id="PoaCollectAll">${getTextForUI("collectAllButton", "elementText")}</button>`);
+            const divTooltip = $(`<div class="tooltipHH" style="position: absolute;top: -30px;left: 730px;width: 110px;font-size: small;"><span class="tooltipHHtext">${getTextForUI("collectAllButton", "tooltip")}</span></div>`);
             divTooltip.append(button);
-            $('#poa-content .girls').append(divTooltip);
+            $('#poa-content').append(divTooltip);
             button.one('click', () => {
                 PathOfAttraction.goAndCollect(true);
             });
-            */
         }
     }
     static _getClaimableRewards(path) {
@@ -7489,6 +7486,8 @@ class Missions {
                 }
                 if (!allGood) {
                     LogUtils_logHHAuto("Mission ongoing waiting it ends.");
+                    if (checkTimer('nextMissionTime'))
+                        setTimer('nextMissionTime', randomInterval(15, 30));
                     return true;
                 }
                 if (debugEnabled)
