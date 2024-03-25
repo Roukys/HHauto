fe65b45dfa4822a2da2e8a6cfbd108abf8f05164
deuxge <deuxge@users.noreply.github.com>
2024-03-25 21:41:30 +0100

    Add unit tests

--- spec/Module/Events/PathOfAttraction.spec.ts
+++ spec/Module/Events/PathOfAttraction.spec.ts
@@ -1,8 +1,22 @@
 import { PathOfAttraction } from "../../../src/Module/index";
+import { HHStoredVarPrefixKey } from "../../../src/config/HHStoredVars";
 
 describe("PathOfAttraction", function () {
+    afterEach(() => {
+        localStorage.clear();
+        sessionStorage.clear();
+    });
+
     describe("styles", function () {
         it("default", function () {
+            expect(() => PathOfAttraction.styles()).not.toThrow()
+        });
+        it("PoAMaskRewards", function () {
+            localStorage.setItem(HHStoredVarPrefixKey + "Setting_plusEventMythicSandalWood", 'true');
+            expect(() => PathOfAttraction.styles()).not.toThrow()
+        });
+        it("showRewardsRecap", function () {
+            localStorage.setItem(HHStoredVarPrefixKey + "Setting_plusEventMythicSandalWood", 'true');
             expect(() => PathOfAttraction.styles()).not.toThrow()
         });
     });
