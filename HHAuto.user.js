e457623291e455efc0fcfcc0dc2c0d5cf6ba7944
deuxge <deuxge@users.noreply.github.com>
2024-07-23 22:42:04 +0200

    Avoid error when last girl collected #1305

--- HHAuto.user.js
+++ HHAuto.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         HaremHeroes Automatic++
 // @namespace    https://github.com/Roukys/HHauto
-// @version      7.14.3
+// @version      7.14.4
 // @description  Open the menu in HaremHeroes(topright) to toggle AutoControlls. Supports AutoSalary, AutoContest, AutoMission, AutoQuest, AutoTrollBattle, AutoArenaBattle and AutoPachinko(Free), AutoLeagues, AutoChampions and AutoStatUpgrades. Messages are printed in local console.
 // @author       JD and Dorten(a bit), Roukys, cossname, YotoTheOne, CLSchwab, deuxge, react31, PrimusVox, OldRon1977, tsokh, UncleBob800
 // @match        http*://*.haremheroes.com/*
@@ -8594,7 +8594,8 @@ class HaremSalary {
                                 waitBetweenGirlsTime = randomInterval(1200, 2000);
                                 girlsToCollectBeforeWait = randomInterval(6, 12);
                             }
-                            LogUtils_logHHAuto(`Next girl collection in ${waitBetweenGirlsTime}ms after n°${Clicked[0]}`);
+                            if (Clicked.length > 0)
+                                LogUtils_logHHAuto(`Next girl collection in ${waitBetweenGirlsTime}ms after n°${Clicked[0]}`);
                             setTimeout(ClickThem, waitBetweenGirlsTime);
                             if (window.top)
                                 window.top.postMessage({ ImAlive: true }, '*');
