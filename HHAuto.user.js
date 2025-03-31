bfb0441151c5379d91ddbf24b0c150a20c502eb3
deuxge <deuxge@users.noreply.github.com>
2025-03-31 14:44:27 +0200

    Add button on edit team to remove girl equipment #1364

--- HHAuto.user.js
+++ HHAuto.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         HaremHeroes Automatic++
 // @namespace    https://github.com/Roukys/HHauto
-// @version      7.21.3
+// @version      7.22.0
 // @description  Open the menu in HaremHeroes(topright) to toggle AutoControlls. Supports AutoSalary, AutoContest, AutoMission, AutoQuest, AutoTrollBattle, AutoArenaBattle and AutoPachinko(Free), AutoLeagues, AutoChampions and AutoStatUpgrades. Messages are printed in local console.
 // @author       JD and Dorten(a bit), Roukys, cossname, YotoTheOne, CLSchwab, deuxge, react31, PrimusVox, OldRon1977, tsokh, UncleBob800
 // @match        http*://*.haremheroes.com/*
@@ -418,6 +418,7 @@ HHAuto_ToolTips.en['PachinkoFillOrbs'] = { version: "5.6.134", elementText: 'Fil
 HHAuto_ToolTips.en['PachinkoOrbsSpent'] = { version: "7.3.5", elementText: 'Orbs spent:', tooltip: "" };
 HHAuto_ToolTips.en['ChangeTeamButton'] = { version: "5.6.24", elementText: "Current Best", tooltip: "Get list of top 16 girls for your team." };
 HHAuto_ToolTips.en['ChangeTeamButton2'] = { version: "5.6.24", elementText: "Possible Best", tooltip: "Get list of top 16 girls for your team if they are Max Lv & Aff" };
+HHAuto_ToolTips.en['UnequipAll'] = { version: "7.22.0", elementText: "Unequip All", tooltip: "Unequip all girls equipment" };
 HHAuto_ToolTips.en['AssignTopTeam'] = { version: "5.6.24", elementText: "Assign first 7", tooltip: "Put the first 7 ones in the team." };
 HHAuto_ToolTips.en['ExportGirlsData'] = { version: "5.6.24", elementText: "⤓", tooltip: "Export Girls data." };
 HHAuto_ToolTips.en['autoFreeBundlesCollect'] = { version: "5.16.0", elementText: "Collect free bundles", tooltip: "Collect free bundles." };
@@ -13327,160 +13328,195 @@ Spreadsheet.POPUP_SELECTOR = '#blessings_popup .blessings_wrapper';
 
 
 
+
 class TeamModule {
     static resetTeam() {
-        $('#clear-team').click();
+        $('#clear-team').trigger('click');
     }
     static validateTeam() {
-        $('#validate-team').click();
+        $('#validate-team').trigger('click');
     }
     static moduleChangeTeam() {
         if (document.getElementById("ChangeTeamButton") !== null || document.getElementById("ChangeTeamButton2") !== null) {
             return;
         }
-        let ChangeTeamButton = '<div style="position: absolute;left: 60%;top: 110px;width:60px;z-index:10" class="tooltipHH"><span class="tooltipHHtext">' + getTextForUI("ChangeTeamButton", "tooltip") + '</span><label style="font-size:small" class="myButton" id="ChangeTeamButton">' + getTextForUI("ChangeTeamButton", "elementText") + '</label></div>';
-        let ChangeTeamButton2 = '<div style="position: absolute;left: 60%;top: 180px;width:60px;z-index:10" class="tooltipHH"><span class="tooltipHHtext">' + getTextForUI("ChangeTeamButton2", "tooltip") + '</span><label style="font-size:small" class="myButton" id="ChangeTeamButton2">' + getTextForUI("ChangeTeamButton2", "elementText") + '</label></div>';
+        const buttonStyles = 'position: absolute;left: 60%;width:60px;z-index:10';
+        const ChangeTeamButton = hhButton('ChangeTeamButton', 'ChangeTeamButton', buttonStyles + ';top: 110px', 'font-size:small');
+        const ChangeTeamButton2 = hhButton('ChangeTeamButton2', 'ChangeTeamButton2', buttonStyles + ';top: 160px', 'font-size:small');
+        const UnequipAll = hhButton('UnequipAll', 'UnequipAll', buttonStyles + ';top: 210px', 'font-size:small');
         GM_addStyle('.topNumber{top: 2px;left: 12px;width: 100%;position: absolute;text-shadow: 1px 1px 1px black, -1px -1px 1px black;}');
         $("#contains_all section").append(ChangeTeamButton);
         $("#contains_all section").append(ChangeTeamButton2);
-        function assignTopTeam() {
-            setStoredValue(HHStoredVarPrefixKey + "Temp_autoLoop", "false");
-            LogUtils_logHHAuto("setting autoloop to false");
-            function selectFromHaremBest(i, best) {
-                let girlToSelect = best ? i : i + 7;
-                //console.log(i,girlToSelect,best);
-                let selectedGirl = $('#contains_all section ' + ConfigHelper.getHHScriptVars("IDpanelEditTeam") + ' .harem-panel .panel-body .topNumber[position="' + girlToSelect + '"]');
-                selectedGirl.click();
-                //console.log(selectedGirl);
-                if ($('.topNumber').length > girlToSelect && i < 7) {
-                    setTimeout(function () { assignToTeam(i + 1, best); }, randomInterval(300, 600));
-                }
-                else {
-                    if (!best) {
-                        assignToTeam(1, true);
-                    }
-                    else {
-                        TeamModule.validateTeam();
-                    }
-                }
-            }
-            function assignToTeam(i = 1, best = false) {
-                let position = i - 1;
-                let selectedPosition = $('#contains_all section .player-panel .player-team .team-hexagon .team-member-container.selectable[data-team-member-position="' + position + '"]');
-                selectedPosition.click();
-                //console.log(selectedPosition);
-                setTimeout(function () { selectFromHaremBest(i, best); }, randomInterval(300, 600));
-            }
-            let topNumbers = $('.topNumber');
-            if (topNumbers.length > 0) {
-                TeamModule.resetTeam();
-                assignToTeam(1, true); // true = jump to best team directly
-            }
+        $("#contains_all section").append(UnequipAll);
+        $("#ChangeTeamButton").on("click", () => { TeamModule.setTopTeam(1); });
+        $("#ChangeTeamButton2").on("click", () => { TeamModule.setTopTeam(2); });
+        $("#UnequipAll").on("click", TeamModule.unequipAllGirls);
+    }
+    static unequipAllGirls() {
+        LogUtils_logHHAuto('Unequip');
+        $("#UnequipAll").attr('disabled', 'disabled');
+        const girlId = TeamModule.getFirstSelectedGirlId();
+        if (isNaN(girlId) || girlId < 0) {
+            LogUtils_logHHAuto('Error: can\'t get mandatory girl id, cancel action');
+            return;
         }
-        function setTopTeam(sumFormulaType) {
-            let arr = $('div[id_girl]');
-            let numTop = 16;
-            if (numTop > arr.length)
-                numTop = arr.length;
-            let deckID = [];
-            let deckStat = [];
-            for (let z = 0; z < numTop; z++) {
-                deckID.push(-1);
-                deckStat.push(-1);
-            }
-            let levelPlayer = Number(HeroHelper.getLevel());
-            for (let i = arr.length - 1; i > -1; i--) {
-                let gID = Number($(arr[i]).attr('id_girl'));
-                const tooltipData = $('.girl_img', $(arr[i])).attr(ConfigHelper.getHHScriptVars('girlToolTipData')) || '';
-                //const girlData = Harem.getGirlData(gID);
-                if (tooltipData == '') {
-                    LogUtils_logHHAuto('ERROR, no girl information found');
-                    return;
-                }
-                let obj = JSON.parse(tooltipData);
-                //sum formula
-                let tempGrades = obj.graded2;
-                //console.log(obj,tempGrades);
-                let countTotalGrades = (tempGrades.match(/<g/g) || []).length;
-                let countFreeGrades = (tempGrades.match(/grey/g) || []).length;
-                let currentStat = obj.caracs.carac1 + obj.caracs.carac2 + obj.caracs.carac3;
-                //console.log(currentStat);
-                if (sumFormulaType == 1) {
-                    currentStat = obj.caracs.carac1 + obj.caracs.carac2 + obj.caracs.carac3;
-                }
-                else if (sumFormulaType == 2) {
-                    currentStat = (obj.caracs.carac1 + obj.caracs.carac2 + obj.caracs.carac3) / obj.level * levelPlayer / (1 + 0.3 * (countTotalGrades - countFreeGrades)) * (1 + 0.3 * (countTotalGrades));
-                }
-                //console.log(obj.level,levelPlayer,countTotalGrades,countFreeGrades);
-                //console.log(currentStat);
-                let lowNum = 0; //num
-                let lowStat = deckStat[0]; //stat
-                for (let j = 1; j < deckID.length; j++) {
-                    if (deckStat[j] < lowStat) {
-                        lowNum = j;
-                        lowStat = deckStat[j];
-                    }
-                }
-                if (lowStat < currentStat) {
-                    deckID[lowNum] = gID;
-                    deckStat[lowNum] = currentStat;
-                }
-            }
-            let tmpID = 0;
-            let tmpStat = 0;
-            //console.log(deckStat,deckID);
-            for (let i = 0; i < deckStat.length; i++) {
-                for (let j = i; j < deckStat.length; j++) {
-                    if (deckStat[j] > deckStat[i]) {
-                        tmpID = deckID[i];
-                        tmpStat = deckStat[i];
-                        deckID[i] = deckID[j];
-                        deckStat[i] = deckStat[j];
-                        deckID[j] = tmpID;
-                        deckStat[j] = tmpStat;
-                    }
-                }
-            }
-            //console.log(deckStat,deckID);
-            for (let i = arr.length - 1; i > -1; i--) {
-                let gID = Number($(arr[i]).attr('id_girl'));
-                if (!deckID.includes(gID)) {
-                    arr[i].style.display = "none";
-                }
-                else {
-                    arr[i].style.display = "";
-                }
-            }
-            let mainTeamPanel = $(ConfigHelper.getHHScriptVars("IDpanelEditTeam") + ' .change-team-panel .panel-body > .harem-panel-girls');
-            for (let j = 0; j < deckID.length; j++) {
-                let newDiv;
-                let arrSort = $('div[id_girl=' + deckID[j] + ']');
-                if ($(arrSort[0]).find('.topNumber').length == 0) {
-                    newDiv = document.createElement("div");
-                    newDiv.className = "topNumber";
-                    arrSort[0].prepend(newDiv);
-                }
-                else {
-                    newDiv = $(arrSort[0]).find('.topNumber')[0];
-                }
-                $(arrSort[0]).find('.topNumber')[0];
-                newDiv.innerText = j + 1;
-                newDiv.setAttribute('position', j + 1);
-                // Go to girl update page on double click
-                newDiv.setAttribute("ondblclick", "window.location.href='/characters/" + deckID[j] + "'");
-                mainTeamPanel.append(arrSort[0]);
-            }
-            if (document.getElementById("AssignTopTeam") !== null) {
-                return;
+        const currentPage = window.location.pathname + window.location.search;
+        // change referer
+        //logHHAuto('change referer to ' + '/characters/' + girlId);
+        window.history.replaceState(null, '', addNutakuSession('/characters/' + girlId));
+        var params1 = {
+            action: "girl_equipment_unequip_all_girls"
+        };
+        getHHAjax()(params1, function (data) {
+            $("#UnequipAll").removeAttr('disabled');
+            // change referer
+            //logHHAuto('change referer back to ' + currentPage);
+            window.history.replaceState(null, '', addNutakuSession(currentPage));
+            setTimeout(function () { location.reload(); }, randomInterval(200, 500));
+        });
+    }
+    static getFirstSelectedGirlId() {
+        const selectedPosition = $('#contains_all section .player-panel .player-team .team-hexagon .team-member-container.selectable[data-team-member-position="0"]');
+        if (selectedPosition.length > 0) {
+            return Number(selectedPosition.attr('data-girl-id'));
+        }
+        return -1;
+    }
+    static assignTopTeam() {
+        setStoredValue(HHStoredVarPrefixKey + "Temp_autoLoop", "false");
+        LogUtils_logHHAuto("setting autoloop to false");
+        function selectFromHaremBest(i, best) {
+            let girlToSelect = best ? i : i + 7;
+            //console.log(i,girlToSelect,best);
+            let selectedGirl = $('#contains_all section ' + ConfigHelper.getHHScriptVars("IDpanelEditTeam") + ' .harem-panel .panel-body .topNumber[position="' + girlToSelect + '"]');
+            selectedGirl.click();
+            //console.log(selectedGirl);
+            if ($('.topNumber').length > girlToSelect && i < 7) {
+                setTimeout(function () { assignToTeam(i + 1, best); }, randomInterval(300, 600));
             }
             else {
-                let AssignTopTeam = '<div style="position: absolute;top: 92px;width:100px;z-index:10;margin-left:90px" class="tooltipHH"><span class="tooltipHHtext">' + getTextForUI("AssignTopTeam", "tooltip") + '</span><label style="font-size:small" class="myButton" id="AssignTopTeam">' + getTextForUI("AssignTopTeam", "elementText") + '</label></div>';
-                $("#contains_all section " + ConfigHelper.getHHScriptVars("IDpanelEditTeam") + " .harem-panel .panel-body").append(AssignTopTeam);
-                $("#AssignTopTeam").on("click", assignTopTeam);
+                if (!best) {
+                    assignToTeam(1, true);
+                }
+                else {
+                    TeamModule.validateTeam();
+                }
             }
         }
-        $("#ChangeTeamButton").on("click", () => { setTopTeam(1); });
-        $("#ChangeTeamButton2").on("click", () => { setTopTeam(2); });
+        function assignToTeam(i = 1, best = false) {
+            let position = i - 1;
+            let selectedPosition = $('#contains_all section .player-panel .player-team .team-hexagon .team-member-container.selectable[data-team-member-position="' + position + '"]');
+            selectedPosition.click();
+            //console.log(selectedPosition);
+            setTimeout(function () { selectFromHaremBest(i, best); }, randomInterval(300, 600));
+        }
+        let topNumbers = $('.topNumber');
+        if (topNumbers.length > 0) {
+            TeamModule.resetTeam();
+            assignToTeam(1, true); // true = jump to best team directly
+        }
+    }
+    static setTopTeam(sumFormulaType) {
+        let arr = $('div[id_girl]');
+        let numTop = 16;
+        if (numTop > arr.length)
+            numTop = arr.length;
+        let deckID = [];
+        let deckStat = [];
+        for (let z = 0; z < numTop; z++) {
+            deckID.push(-1);
+            deckStat.push(-1);
+        }
+        let levelPlayer = Number(HeroHelper.getLevel());
+        for (let i = arr.length - 1; i > -1; i--) {
+            let gID = Number($(arr[i]).attr('id_girl'));
+            const tooltipData = $('.girl_img', $(arr[i])).attr(ConfigHelper.getHHScriptVars('girlToolTipData')) || '';
+            //const girlData = Harem.getGirlData(gID);
+            if (tooltipData == '') {
+                LogUtils_logHHAuto('ERROR, no girl information found');
+                return;
+            }
+            let obj = JSON.parse(tooltipData);
+            //sum formula
+            let tempGrades = obj.graded2;
+            //console.log(obj,tempGrades);
+            let countTotalGrades = (tempGrades.match(/<g/g) || []).length;
+            let countFreeGrades = (tempGrades.match(/grey/g) || []).length;
+            let currentStat = obj.caracs.carac1 + obj.caracs.carac2 + obj.caracs.carac3;
+            //console.log(currentStat);
+            if (sumFormulaType == 1) {
+                currentStat = obj.caracs.carac1 + obj.caracs.carac2 + obj.caracs.carac3;
+            }
+            else if (sumFormulaType == 2) {
+                currentStat = (obj.caracs.carac1 + obj.caracs.carac2 + obj.caracs.carac3) / obj.level * levelPlayer / (1 + 0.3 * (countTotalGrades - countFreeGrades)) * (1 + 0.3 * (countTotalGrades));
+            }
+            //console.log(obj.level,levelPlayer,countTotalGrades,countFreeGrades);
+            //console.log(currentStat);
+            let lowNum = 0; //num
+            let lowStat = deckStat[0]; //stat
+            for (let j = 1; j < deckID.length; j++) {
+                if (deckStat[j] < lowStat) {
+                    lowNum = j;
+                    lowStat = deckStat[j];
+                }
+            }
+            if (lowStat < currentStat) {
+                deckID[lowNum] = gID;
+                deckStat[lowNum] = currentStat;
+            }
+        }
+        let tmpID = 0;
+        let tmpStat = 0;
+        //console.log(deckStat,deckID);
+        for (let i = 0; i < deckStat.length; i++) {
+            for (let j = i; j < deckStat.length; j++) {
+                if (deckStat[j] > deckStat[i]) {
+                    tmpID = deckID[i];
+                    tmpStat = deckStat[i];
+                    deckID[i] = deckID[j];
+                    deckStat[i] = deckStat[j];
+                    deckID[j] = tmpID;
+                    deckStat[j] = tmpStat;
+                }
+            }
+        }
+        //console.log(deckStat,deckID);
+        for (let i = arr.length - 1; i > -1; i--) {
+            let gID = Number($(arr[i]).attr('id_girl'));
+            if (!deckID.includes(gID)) {
+                arr[i].style.display = "none";
+            }
+            else {
+                arr[i].style.display = "";
+            }
+        }
+        let mainTeamPanel = $(ConfigHelper.getHHScriptVars("IDpanelEditTeam") + ' .change-team-panel .panel-body > .harem-panel-girls');
+        for (let j = 0; j < deckID.length; j++) {
+            let newDiv;
+            let arrSort = $('div[id_girl=' + deckID[j] + ']');
+            if ($(arrSort[0]).find('.topNumber').length == 0) {
+                newDiv = document.createElement("div");
+                newDiv.className = "topNumber";
+                arrSort[0].prepend(newDiv);
+            }
+            else {
+                newDiv = $(arrSort[0]).find('.topNumber')[0];
+            }
+            $(arrSort[0]).find('.topNumber')[0];
+            newDiv.innerText = j + 1;
+            newDiv.setAttribute('position', j + 1);
+            // Go to girl update page on double click
+            newDiv.setAttribute("ondblclick", "window.location.href='/characters/" + deckID[j] + "'");
+            mainTeamPanel.append(arrSort[0]);
+        }
+        if (document.getElementById("AssignTopTeam") !== null) {
+            return;
+        }
+        else {
+            let AssignTopTeam = '<div style="position: absolute;top: 92px;width:100px;z-index:10;margin-left:90px" class="tooltipHH"><span class="tooltipHHtext">' + getTextForUI("AssignTopTeam", "tooltip") + '</span><label style="font-size:small" class="myButton" id="AssignTopTeam">' + getTextForUI("AssignTopTeam", "elementText") + '</label></div>';
+            $("#contains_all section " + ConfigHelper.getHHScriptVars("IDpanelEditTeam") + " .harem-panel .panel-body").append(AssignTopTeam);
+            $("#AssignTopTeam").on("click", TeamModule.assignTopTeam);
+        }
     }
 }
 
@@ -14547,10 +14583,10 @@ function maskInactiveMenus() {
         }
     }
 }
-function hhButton(textKeyId, buttonId) {
-    return `<div class="tooltipHH">`
+function hhButton(textKeyId, buttonId, mainStyle = '', labelSyle = '') {
+    return `<div ${mainStyle ? 'style="' + mainStyle + '"' : ''} class="tooltipHH" >`
         + `<span class="tooltipHHtext">${getTextForUI(textKeyId, "tooltip")}</span>`
-        + `<label class="myButton" id="${buttonId}">${getTextForUI(textKeyId, "elementText")}</label>`
+        + `<label ${labelSyle ? 'style="' + labelSyle + '"' : ''} class="myButton" id="${buttonId}">${getTextForUI(textKeyId, "elementText")}</label>`
         + `</div>`;
 }
 function hhMenuSwitch(textKeyAndInputId, isEnabledDivId = '', isKobanSwitch = false, isStylingSwitch = false) {
